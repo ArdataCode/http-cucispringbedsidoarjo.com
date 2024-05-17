21afb0e73663d878fc8bc5a906cb3e7bec317e68
ArdataCode <ardata.indonesia@gmail.com>
2024-05-17 16:21:57 +0700

    mittttt

---
+++ malcare-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/ujianori/cucispringbedsidoarjo.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/ujianori/cucispringbedsidoarjo.com/wp-content/mc_data/');
+	define("MCCONFKEY", 'a060cfb7d948d8986e0c7e3b0d9544e9');
+	include_once('/home/ujianori/cucispringbedsidoarjo.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         'tucHEYWVuzcuBPxAKAnn1igMd6WIbWXxImV48mRWdMGKYLnied8xsmcqf7pSTt8W');
+define('SECURE_AUTH_KEY',  'sQE2sEp56ibf0QBCTu34DpT4Ddy2eupe0NLoaxXp7hua4Peu3vUdyVLnh6AFTn1J');
+define('LOGGED_IN_KEY',    'uYYVmMoCDSeR79EBAwve8Cr4YtptoIsGA9cqC3ih6s3VN8SBBPy0w81njcb1VFFV');
+define('NONCE_KEY',        'S7aAdjzvGzPyEQzHbyG8TcVabrcFX6nqpXjtBPXR1pFUqRhyBu5WCQ6KD1BWjYjH');
+define('AUTH_SALT',        'siLXx3TsdSWT3C2zemaMR0Kh5PCWwQKc8cXiNEAuA5ApHI8qAzmcNsh1xMrJHBmw');
+define('SECURE_AUTH_SALT', 'E88oUtqmGMKC18ibFcvxyjFB1CbKxyP4r5xniDpgj7QrKIYBzS9TD19SRfvSG0fU');
+define('LOGGED_IN_SALT',   'oYfrAIJiEYByR7TMnxpMDcFM4vQxXou4mpopCr66LPtDYg5W4Vc2XIRbVEtYbzAg');
+define('NONCE_SALT',       'gD3CItqsbUNT3oyWxNLbHWqT16uujfNK7Y52tzxnTSqvTaECvRDVr5re155J1hxB');
